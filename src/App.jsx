483c4617bced2443f7481caf5826162ba84c4206
Dip3102001 <dip3102001@gmail.com>
2025-06-25 16:47:46 -0400

    Updated: Entry point

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import ChatApp from './components/ChatApp';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <ChatApp>
+      </ChatApp>      
     </>
   )
 }
